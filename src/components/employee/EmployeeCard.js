61f056231afdb686e36a88e4157ada98da0384a4
Trinity Terry <trinitycterry@gmail.com>
2020-02-21 13:55:17 -0600

    part1/part2

---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const EmployeeCard = () => {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h3>
+          Name: John Smith
+        </h3>
+        <p>Position: Cashier</p>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeCard;
---
+++ src/components/owner/OwnerCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const OwnerCard = () => {
+  return (
+    <div>
+        <h2>Name: Jane Smith</h2>
+        <p>Dog: Doodles</p>
+    </div>
+  );
+};
+
+export default OwnerCard;
